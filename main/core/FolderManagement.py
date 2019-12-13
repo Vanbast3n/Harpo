9bb44c17183f3a50a76ef8b9593a14d3b3c5f223
Ale <alerodfer8@gmail.com>
2019-12-13 22:08:10 +0100

    getAllMoviesInTheDirectory method and test added

--- main/core/FolderManagement.py
+++ main/core/FolderManagement.py
@@ -28,8 +28,24 @@ class FolderManagement():
         for item in listWithMovies:
             if ".mkv" not in item and ".avi" not in item and ".db" not in item:
                 listWithoutMovies.append(item)
-
         return listWithoutMovies
+
+    def getAllMoviesInTheDirectory(self, directory):
+
+        movieList = self.getMovieList(directory)
+        folderList = self.getFolderList(directory)
+
+        if len(folderList) > 0:
+            movieList1 = []
+            for item in folderList:
+                movieList1 = self.getMovieList(directory + "/" + item)
+                print(directory + "/" + item)
+                print(len(movieList1))
+                for movie in movieList1:
+                    movieList.append(movie)
+
+        return movieList
+
 
 
 
--- tests/FolderManage.py
+++ tests/FolderManage.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from main.core.FolderManagement import FolderManagement
 from main.common.Constants import Constans
 
@@ -8,15 +9,25 @@ class FolderManage(unittest.TestCase):
         cte = Constans()
         fm = FolderManagement()
         directory = cte.FAKE_FILMS_PATH
+        path, dirs, files = next(os.walk(directory))
 
         numberOfMovies = len(fm.getMovieList(directory))
-        self.assertEqual(numberOfMovies,37,"There is no all the movies of the directory in the list {a}".format(a=numberOfMovies))
+        self.assertEqual(numberOfMovies,len(files)-1,"There is no all the movies of the directory in the list {a}".format(a=numberOfMovies))
 
 
     def test_getFolderList(self):
         cte = Constans()
         fm = FolderManagement()
         directory = cte.FAKE_FILMS_PATH
+        path, dirs, files = next(os.walk(directory))
 
         numberOfFolders = len(fm.getFolderList(directory))
-        self.assertEqual(numberOfFolders,15,"There is no all the folders of the directory in the list {a}".format(a=numberOfFolders))
+        self.assertEqual(numberOfFolders,len(dirs),"There is no all the folders of the directory in the list {a}".format(a=numberOfFolders))
+
+    def test_extractAllMovies(self):
+        cte = Constans()
+        fm = FolderManagement()
+        directory = cte.FAKE_FILMS_PATH
+
+        numberOfMoviesTotal = len(fm.getAllMoviesInTheDirectory(directory))
+        self.assertEqual(numberOfMoviesTotal, 190,"There is no all the movies of the directory in the list {a}".format(a=numberOfMoviesTotal))
